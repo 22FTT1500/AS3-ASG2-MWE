d7b82a76f58ccc5f6dad080058c0d7d514b001f2
22FTT1500 <22FTT1500@student.pb.edu.bn>
2024-04-06 15:28:34 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'AS3-ASG2-mwe',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
