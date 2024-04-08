08d3e3871455abc2688b2ce838b75b6d0b42d7f3
22FTT1500 <22FTT1500@student.pb.edu.bn>
2024-04-08 13:21:52 +0800

    add refresh button and link

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -24,6 +24,10 @@ const routes: Routes = [
         loadChildren: () => import('../tab4/tab4.module').then(m => m.Tab4PageModule)
       },
       {
+        path: 'refresh',
+        loadChildren: () => import('../refresh/refresh.module').then( m => m.RefreshPageModule)
+      },
+      {
 
         path: '',
         redirectTo: '/tabs/tab1',
