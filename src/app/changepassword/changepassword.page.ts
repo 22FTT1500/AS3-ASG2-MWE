46f8b36cfc61367352370b6e952e6c617989879b
22FTT1500 <22FTT1500@student.pb.edu.bn>
2024-04-08 12:51:56 +0800

    adding alert for password & linking router

--- src/app/changepassword/changepassword.page.ts
+++ src/app/changepassword/changepassword.page.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-changepassword',
   templateUrl: './changepassword.page.html',
   styleUrls: ['./changepassword.page.scss'],
 })
-export class ChangepasswordPage implements OnInit {
+export class ChangepasswordPage{
+  alertButtons = ['Ok'];
 
-  constructor() { }
+  constructor(private navCtrl: NavController) { }
 
-  ngOnInit() {
+
+  onSave() {
+    // Navigate to tab1 page
+    this.navCtrl.navigateRoot('/tabs/tab1');
+  
+    // Clear input fields by selecting them and setting their value to an empty string
+    const passwordInputs = document.querySelectorAll('input[type="password"]');
+    passwordInputs.forEach(input => {
+      const passwordInput = input as HTMLInputElement; // Cast input to HTMLInputElement
+      passwordInput.value = '';
+    });
   }
-
+  
 }
